e4e73e88fe941d488719db6bf7c9c82068340d14
RobertoGP96 <trolbertogp96@gmail.com>
2025-08-21 17:05:37 -0400

    adding new view components

--- apps/client/src/components/about/price/Pricing.tsx
+++ apps/client/src/components/about/price/Pricing.tsx
@@ -1,4 +1,5 @@
 import { CheckIcon } from "lucide-react"
+import ExchangeRate from "./rage-exchange"
 
 
 const tiers = [
@@ -41,16 +42,7 @@ function classNames(...classes: string[]) {
 
 export default function Pricing() {
   return (
-    <div className="relative isolate px-6 lg:px-8 ">
-      <div aria-hidden="true" className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl">
-        <div
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-          className="mx-auto aspect-1155/678 w-288.75 bg-linear-to-tr from-[#ffab4a] to-[#e67519] opacity-30 dark:opacity-20"
-        />
-      </div>
+    <div className="relative isolate px-6 lg:px-8 flex flex-col gap-y-3 ">
       <div className="mx-auto max-w-4xl text-center">
         <h1 className="font-semibold text-primary">
           Precios</h1>
@@ -136,6 +128,8 @@ export default function Pricing() {
           </div>
         ))}
       </div>
+
+      <ExchangeRate/>
     </div>
   )
 }
---
+++ apps/client/src/components/about/price/rage-exchange.tsx
@@ -0,0 +1,73 @@
+import { ChevronRight } from "lucide-react"
+
+type rateItem = {
+    exchangeFrom: string,
+    exchangeTo: string,
+    rate: number
+}
+
+const rates: rateItem[] = [
+    {
+        exchangeFrom: "USD",
+        exchangeTo: "CUP",
+        rate: 405.00
+    },
+    {
+        exchangeFrom: "USD",
+        exchangeTo: "TCUP",
+        rate: 445.00
+    }, {
+        exchangeFrom: "USD",
+        exchangeTo: "Zelle",
+        rate: 1
+    }
+]
+
+export default function ExchangeRate() {
+    return (
+        <div className="relative isolate px-6 lg:px-8 mt-20 " id="exchange-rates">
+            <div className="mx-auto max-w-4xl text-center">
+                <p className="mt-2 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl dark:text-white">
+                    Tasas de Cambio
+                </p>
+            </div>
+            <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-gray-600 sm:text-xl/8 dark:text-gray-400">
+                Estas son las tasa aplicadas a las diferentes monedas actualmente.
+            </p>
+            <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6">
+                {rates.map((rateItem, index) => (
+                    <div
+                        key={index}
+                        className={`bg-black/10`+' ring-1 ring-gray-500/30 px-6 p-4 flex flex-row justify-between items-center rounded-xl shadow-none sm:mx-8 lg:mx-0 transition-all duration-300 ease-in-out hover:scale-105'}
+                    >
+                        <p className="flex items-baseline gap-x-2 m-0">
+                            <span
+                                className={'text-white text-5xl font-semibold tracking-tight'}
+                            >
+                                $1.00
+                            </span>
+                            <span
+                                className={'text-black text-base bg-primary px-2 rounded-full'}
+                            >
+                                {rateItem.exchangeFrom}
+                            </span>
+                        </p>
+                        <ChevronRight/>
+                        <p className="flex items-baseline gap-x-2">
+                            <span
+                                className={'text-white text-5xl font-semibold tracking-tight'}
+                            >
+                                {"$"+rateItem.rate.toFixed(2)}
+                            </span>
+                            <span
+                                className={'text-black text-base bg-primary px-2 rounded-full'}
+                            >
+                                {rateItem.exchangeTo}
+                            </span>
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
