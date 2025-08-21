e7af70ac36c2e9b4f9e4c09054ba8950fb3c3d4f
RobertoGP96 <trolbertogp96@gmail.com>
2025-08-21 17:05:55 -0400

    adding user nav component

--- apps/client/src/components/navigation/nav-bar.tsx
+++ apps/client/src/components/navigation/nav-bar.tsx
@@ -1,14 +1,19 @@
 import { Compass, Headset, Home, LogIn, UserPlus } from "lucide-react";
 import { Button } from "../ui/button";
-import { NavLink } from "react-router";
+import { NavLink, useLocation,} from "react-router";
+import useAuth from "@/hooks/auth/useAuth";
+import { NavUser } from "./user-nav";
+import LoadingSpinner from "../utils/LoadingSpinner";
 
 const NavBar = () => {
+    const { isLoading, isAuthenticated } = useAuth()
 
     const navigation = [
-        { name: 'Inicio', href: '/', icon: Home },
+        { name: 'Inicio', href: 'home', icon: Home },
         { name: 'Saber más', href: 'pricing', icon: Compass },
         { name: 'Contáctanos', href: 'contact', icon: Headset },
     ]
+    const location= useLocation()
 
     return (
         <header className="w-full">
@@ -19,7 +24,7 @@ const NavBar = () => {
                 <div className="hidden lg:flex lg:gap-x-12 ">
                     {navigation.map((item) => (
                         <NavLink to={item.href}>
-                            <div className="flex flex-row justify-center items-center gap-1 text-white hover:text-primary ">
+                            <div className={`flex flex-row justify-center items-center gap-1  hover:text-primary ${location.pathname.includes(item.href)?"text-primary":"text-white"}` }>
                                 <item.icon className='h-4 w-4' />
                                 <span className="text-sm/6 font-semibold  ">
                                     {item.name}
@@ -29,20 +34,31 @@ const NavBar = () => {
                     ))}
                 </div>
                 <div className="flex flex-row justify-center items-center gap-2 lg:flex lg:flex-1 lg:justify-end">
-                    <NavLink to={"/login"}>
+                    {isLoading &&
+                        <LoadingSpinner />
+                    }
+                    {!isLoading && isAuthenticated ? (<>
+                        <div className="flex flex-row justify-center items-end">
+                            <NavUser />
+                        </div>
+                    </>) : (<>
+                        <NavLink to={"/login"}>
+                            <Button variant={"outline"} className="cursor-pointer">
+                                <LogIn />
+                                Inicia Sesión
+                            </Button>
+                        </NavLink>
+                        <span>ó</span>
+                        <NavLink to={"/register"}>
+                            <Button variant={"outline"} className="cursor-pointer">
+                                <UserPlus />
+                                Registrate
+                            </Button>
+                        </NavLink>
+                    </>
+                    )
+                    }
 
-                        <Button variant={"outline"} className="cursor-pointer">
-                            <LogIn />
-                            Inicia Sesión
-                        </Button>
-                    </NavLink>
-                    <span>ó</span>
-                    <NavLink to={"/register"}>
-                        <Button variant={"outline"} className="cursor-pointer">
-                            <UserPlus />
-                            Registrate
-                        </Button>
-                    </NavLink>
                 </div>
             </nav>
 
--- apps/client/src/components/navigation/user-nav.tsx
+++ apps/client/src/components/navigation/user-nav.tsx
@@ -1,8 +1,8 @@
 import { useAuthUser } from "@/hooks/auth/useAuth";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { LogOut, Settings, ShoppingCart, User } from "lucide-react";
-import { useNavigate } from "react-router";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Avatar, AvatarFallback} from "../ui/avatar";
+import { LogOut, ShoppingCart, User } from "lucide-react";
+import { NavLink, useNavigate } from "react-router";
 
 export function NavUser() {
     const auth = useAuthUser()
@@ -10,49 +10,39 @@ export function NavUser() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <button className="w-full flex items-center gap-3 px-4 py-3 rounded-xl hover:bg-gray-700/50 transition-all duration-200 group">
-                    <Avatar className="h-10 w-10 rounded-xl ring-2 ring-orange-500/20">
-                        <AvatarImage src="" alt="Administrador" />
-                        <AvatarFallback className="rounded-xl bg-gradient-to-r from-orange-400 to-amber-500 text-gray-900 font-semibold">
+                <div className="w-full flex items-center gap-3 px-4 py-3 rounded-xl hover:bg-gray-800/50 transition-all duration-200 group">
+                    <Avatar className="h-10 w-10 rounded-xl">
+                        <AvatarFallback className="rounded-full bg-gradient-to-r from-orange-400 to-amber-500 text-gray-900 font-semibold">
                             {auth ? auth?.name.charAt(0).toUpperCase() + auth?.last_name.charAt(0).toUpperCase() : "N/A"}
                         </AvatarFallback>
                     </Avatar>
                     <div className="flex-1 text-left">
-                        <span className="block font-semibold text-base text-white">{auth?.full_name}</span>
-                        <span className="block text-sm text-gray-400">{auth?.phone_number}</span>
+                        <span className="block font-semibold text-base text-white">{auth?.full_name || "Username"}</span>
+                        <span className="block text-sm text-gray-400">{auth?.phone_number || "phone number"}</span>
                     </div>
-                </button>
+                </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent
                 className="w-56 rounded-xl border-0 shadow-xl bg-background"
-                side="right"
+                side="bottom"
                 align="end"
                 sideOffset={8}
             >
-                <DropdownMenuLabel className="p-0 font-normal">
-                    <div className="flex items-center gap-3 px-3 py-3 text-left text-sm">
-                        <Avatar className="h-10 w-10 rounded-xl ring-2 ring-orange-500/20">
-                            <AvatarImage src="" alt="Administrador" />
-                            <AvatarFallback className="rounded-xl bg-gradient-to-r from-orange-400 to-amber-500 text-gray-900 font-semibold">AD</AvatarFallback>
-                        </Avatar>
-                        <div className="grid flex-1 text-left text-sm leading-tight">
-                            <span className="truncate font-semibold text-base text-gray-800">Administrador</span>
-                            <span className="truncate text-sm text-gray-500">admin@example.com</span>
-                        </div>
-                    </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator className="bg-gray-100" />
-                <DropdownMenuItem className="gap-3 px-3 py-3 text-base cursor-pointer hover:bg-orange-50 group" onClick={() => navigate("/profile")}>
-                    <User className="h-5 w-5 text-gray-500 group-hover:text-orange-600" />
-                    <span className="group-hover:text-orange-700">Perfil</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="gap-3 px-3 py-3 text-base cursor-pointer hover:bg-orange-50 group" onClick={() => navigate("/settings")} >
-                    <ShoppingCart className="h-5 w-5 text-gray-500 group-hover:text-orange-600" />
-                    <span className="group-hover:text-orange-700">Configuración</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator className="bg-gray-100" />
-                <DropdownMenuItem className="gap-3 px-3 py-3 text-base text-red-600 focus:text-red-600 cursor-pointer hover:bg-red-50">
-                    <LogOut className="h-5 w-5" />
+                <NavLink to={"/porfile"}>
+                    <DropdownMenuItem className="gap-3 px-3 py-3 text-base cursor-pointer" onClick={() => navigate("/profile")}>
+                        <User className="h-5 w-5 text-primary" />
+                        <span className="text-gray-300">Perfil</span>
+                    </DropdownMenuItem>
+                </NavLink>
+                <NavLink to={"/porfile"}>
+                    <DropdownMenuItem className="gap-3 px-3 py-3 text-base cursor-pointer" onClick={() => navigate("/settings")} >
+                        <ShoppingCart className="h-5 w-5 text-primary" />
+                        <span className="text-gray-300">Pedidos</span>
+                    </DropdownMenuItem>
+                </NavLink>
+                <DropdownMenuSeparator className="bg-gray-800" />
+                <DropdownMenuItem className="gap-3 px-3 py-3 text-base text-red-500 focus:text-red-600 cursor-pointer hover:bg-gray-500/25">
+                    <LogOut className="h-5 w-5 text-red-500" />
                     Cerrar sesión
                 </DropdownMenuItem>
             </DropdownMenuContent>
