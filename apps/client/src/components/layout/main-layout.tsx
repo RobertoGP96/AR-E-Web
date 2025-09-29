eef072757d1433994ed9a7f62a692a2084a89068
RobertoGP96 <trolbertogp96@gmail.com>
2025-09-29 16:57:23 -0400

    refactor(nav-bar, main-layout): ajustar estilos de fondo en la barra de navegación y el diseño principal

--- apps/client/src/components/layout/main-layout.tsx
+++ apps/client/src/components/layout/main-layout.tsx
@@ -5,7 +5,7 @@ import Footer from '../footer/footer';
 const MainLayout = () => {
     return (
         <div className="flex flex-col min-h-screen bg-black/40">
-            <header className="sticky top-0 z-50 w-full">
+            <header className="sticky top-0 z-50 w-full backdrop-blur-md">
                 <NavBar />
             </header>
             <main className="flex-1 w-full">
