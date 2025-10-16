1bc7072c199665656f7c5f6716e0ce8fb609c722
RobertoGP96 <trolbertogp96@gmail.com>
2025-10-16 16:22:01 -0400

    feat: Eliminar animaciones de pulso en el ícono y título de pedidos para mejorar la legibilidad

--- apps/client/src/pages/user-orders.tsx
+++ apps/client/src/pages/user-orders.tsx
@@ -40,8 +40,8 @@ export default function UserOrders() {
                             : 'opacity-0 transform translate-y-8'
                             }`}>
                             <div className="flex items-center gap-x-6">
-                                <ShoppingBag className='h-10 w-10 text-primary animate-pulse' />
-                                <h1 className='text-2xl font-bold text-primary animate-pulse-slow'>
+                                <ShoppingBag className='h-10 w-10 text-primary ' />
+                                <h1 className='text-2xl font-bold text-primary '>
                                     Pedidos
                                 </h1>
                             </div>
