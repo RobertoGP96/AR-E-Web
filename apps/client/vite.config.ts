8b472b28692d8f01e7736fbab85a98bc3c8eaa5b
RobertoGP96 <trolbertogp96@gmail.com>
2025-10-17 21:55:33 -0400

    feat: Agregar directorio público en la configuración de Vite

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
         "@": path.resolve(__dirname, "src"),
       },
     },
+    publicDir: 'public',
     build: {
       outDir: 'dist',
       assetsDir: 'assets',
